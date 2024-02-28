c5bdbe13558970c2e7823fe0b9a85309fed1d541
ssams01 <airjordanfranchize5@gmail.com>
2024-02-28 17:08:22 -0500

    Current ConsoleApp workings

--- FantasyTravelClient/FantasyTravelClient.App/Program.cs
+++ FantasyTravelClient/FantasyTravelClient.App/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using FantasyTravelClient.DTO;
+
+namespace FantasyTravelClient.App
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Your journey has begun");
+
+            
+
+            TravelMenu tmpMenu = new TravelMenu();
+
+            tmpMenu.Menu();
+
+            Console.WriteLine("Thank you for taking us along for the journey. See you next time!");
+        }
+
+        
+
+       
+    }
+}
