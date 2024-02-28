cdf9121ea5bfd6d433f3c0819440a00b9c2adb83
ijortin <ian.j.ortin@gmail.com>
2024-02-28 16:45:54 -0600

    Client work

---
+++ FantasyTravelClient/FantasyTravelClient.DTO/Place.cs
@@ -0,0 +1,36 @@
+﻿namespace FantasyTravelClient.DTO
+{
+    public class Place
+    {
+        // fields
+        public string name { get; set; }
+        public int id { get; set; }
+        private static int idSeed = 1;
+        public string description { get; set; }
+        public int language { get; set; }
+        public int biomeType { get; set; }
+        //constructors
+        public Place() { }
+        public Place(int language, int biomeType, string name = "", string description = "")
+        {
+            this.name = name;
+            this.description = description;
+            this.biomeType = biomeType;
+            this.language = language;
+            id = idSeed++;
+        }
+        public Place(int id, int language, int biomeType, string name = "", string description = "")
+        {
+            this.name = name;
+            this.description = description;
+            this.biomeType = biomeType;
+            this.language = language;
+            this.id = id;
+        }
+        //methods
+        public override string ToString()
+        {
+            return $"Place\nName: {this.name}\nId: {this.id}\nDescription: {this.description}\bBiomeType: {this.biomeType}\n";
+        }
+    }
+}
